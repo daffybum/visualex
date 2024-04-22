2c4809e3f17d000e66068243971c4e167a97700f
daffybum <160607897+daffybum@users.noreply.github.com>
2024-04-22 22:41:37 +0800

    Update __init__.py

--- visualex/__init__.py
+++ visualex/__init__.py
@@ -7,9 +7,11 @@ mysql = MySQL()
 
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET_KEY'] = 'HelloWorld'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-
+    #app.config['SECRET_KEY'] = 'HelloWorld'
+    app.config['MYSQL_HOST'] = 'mysql-srts'
+    app.config['MYSQL_USER'] = 'mysql'
+    app.config['MYSQL_PASSWORD'] = 'password123'
+    app.config['MYSQL_DB'] = 'db'
     app.config['UPLOAD_FOLDER'] = 'visualex/static/uploads/'
     app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
